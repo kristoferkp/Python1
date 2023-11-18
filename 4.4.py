8ddd651f11ed58af78d7f4f91a09394fb66bad13
Kristofer P. <75545969+kristoferkp@users.noreply.github.com>
2023-11-18 10:05:09 +0200

    Update GUI elements in 4.4.py

--- 4.4.py
+++ 4.4.py
@@ -1,16 +1,16 @@
-from tkinter import *
+import tkinter
 
-raam = Tk()
+raam = tkinter.Tk()
 raam.title("Auto")
 raam.geometry("600x600")
 
-tahvel = Canvas(raam, width = 600, height = 600)
+tahvel = tkinter.Canvas(raam, width = 600, height = 600)
 tahvel.pack()
 
-tahvel.create_polygon(200, 100, 150, 150, 150, 225, 400, 225, 400, 100, fill="red")
-tahvel.create_rectangle(275, 125, 200, 175, fill="blue")
-tahvel.create_rectangle(375, 125, 300, 175, fill="blue")
-tahvel.create_oval(200, 200, 250, 250, fill="black")
-tahvel.create_oval(300, 200, 350, 250, fill="black")
+tahvel.create_polygon(200, 100, 150, 150, 150, 225, 400, 225, 400, 100, fill="blue")
+tahvel.create_rectangle(275, 125, 200, 175, fill="lightblue")
+tahvel.create_rectangle(375, 125, 300, 175, fill="lightblue")
+tahvel.create_oval(175, 200, 250, 275, fill="black")
+tahvel.create_oval(300, 200, 375, 275, fill="black")
 
 raam.mainloop()
