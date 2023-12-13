230db844eada5ac193303e6418e94cfa2f685b11
Kristofer P <75545969+kristoferkp@users.noreply.github.com>
2023-12-13 20:56:31 +0200

    Exam upload 😉

---
+++ arvestus.py
@@ -0,0 +1,26 @@
+def raha_eurodes(kastideArv, kiloHind):
+    kiloHind = kiloHind / 100
+    return round(kastideArv * 5 * kiloHind, 1)
+
+
+failiNimi = input("Sisesta faili nimi: ")
+fail = open(failiNimi, "r", encoding="UTF-8")
+andmed= []
+
+for i in fail:
+    andmed.append(int(i.strip()))
+    
+fail.close()
+
+mustikaKiloHind = int(input("Sisesta mustika kilo hind (Sentides): "))
+
+n = 1
+kokku = 0
+
+for i in andmed:
+    i = raha_eurodes(i, mustikaKiloHind)
+    print(f'{n}. päeval sai {i} eurot.')
+    kokku += i
+    n += 1
+
+print(f"Kokku sai: {kokku} eurot.")
